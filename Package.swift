ef9242b576317d25e6c19edfbf9176b48d4b7c29
Taylor Holliday <taylor@audulus.com>
2026-03-15 21:41:08 -0700

    Try not to run out of unwind personality functions
    Hide _lj_err_unwind_dwarf by making the package a dynamic library.

--- Package.swift
+++ Package.swift
@@ -9,9 +9,18 @@ let package = Package(
     products: [
         .library(
             name: "LuaJIT",
-            targets: ["luajit"])
+            
+            // Build LuaJIT as dynamic to hide the _lj_err_unwind_dwarf symbol
+            // so we don't run out of unwind personality functions (only 3 allowed
+            // per library).
+            type: .dynamic,
+            targets: ["luajit", "dummy"])
     ],
     targets: [
+        // Just to convince xcode to build the library as dynamic.
+        .target(
+            name: "dummy",
+            dependencies: ["luajit"]),
         .binaryTarget(
             name: "luajit",
             path: "luajit.xcframework"
---
+++ Sources/dummy/Dummy.swift
@@ -0,0 +1,2 @@
+// We need this to convince xcode to build the library
+// as dynamic.
